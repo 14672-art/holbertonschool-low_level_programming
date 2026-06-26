2583e54f0fa1e0747ee5753fffe305082f19495c
maksoud <14672@holbertonstudents.com>
2026-06-26 13:12:22 +0200

    Adds 0-putchar.c for functions_nested_loops

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Point d'entree du programme
+ *
+ * Description: Affiche _putchar suivi d'un retour a la ligne.
+ * Return: Toujours 0 (Succes)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
