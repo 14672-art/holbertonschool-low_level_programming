320e68ee2b193894c3129cd8dda81a8bd468fbb6
maksoud <14672@holbertonstudents.com>
2026-07-02 15:54:04 +0200

    Fix prototype in main.h and add 1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenates two strings using at most n bytes from src
- * @dest: The destination string
- * @src: The source string
- * @n: The maximum number of bytes to copy from src
+ * _strncat - Concatenates two strings using at most n bytes from src.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copy from src.
  *
- * Return: A pointer to the resulting string dest
+ * Return: A pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int j = 0;
 
-	/* 1. On va jusqu'au bout de dest */
+	/* On avance jusqu'à la fin de dest */
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	/* 2. On copie src dans dest, MAIS avec la limite n */
+	/* On copie src dans dest avec la double condition */
 	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
@@ -27,7 +27,7 @@ char *_strncat(char *dest, char *src, int n)
 		j++;
 	}
 
-	/* 3. On ferme TOUJOURS la chaîne avec le null byte */
+	/* On ajoute le null byte final pour clore proprement */
 	dest[i] = '\0';
 
 	return (dest);
