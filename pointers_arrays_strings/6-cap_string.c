f95abd7ac0df849dbda680e93033609b266e4a3f
maksoud <14672@holbertonstudents.com>
2026-07-03 13:56:28 +0200

    Fix cap_string function

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,51 +1,32 @@
 #include "main.h"
 
 /**
- * is_separator - Vérifie si un caractère est un séparateur de mots.
- * @c: Le caractère à tester.
- *
- * Return: 1 si c'est un séparateur, 0 sinon.
+ * cap_string - Modifie la casse des premieres lettres
+ * @s: La chaine
+ * Return: char pointer
  */
-int is_separator(char c)
+char *cap_string(char *s)
 {
-	char separators[] = " \t\n,;.!?\"(){}";
-	int i = 0;
+	int index = 0;
 
-	while (separators[i] != '\0')
+	while (s[index] != '\0')
 	{
-		if (c == separators[i])
+		if (s[index] >= 'a' && s[index] <= 'z')
 		{
-			return (1);
+			if (index == 0 ||
+			    s[index - 1] == ' ' || s[index - 1] == '\t' ||
+			    s[index - 1] == '\n' || s[index - 1] == ',' ||
+			    s[index - 1] == ';' || s[index - 1] == '.' ||
+			    s[index - 1] == '!' || s[index - 1] == '?' ||
+			    s[index - 1] == '"' || s[index - 1] == '(' ||
+			    s[index - 1] == ')' || s[index - 1] == '{' ||
+			    s[index - 1] == '}')
+			{
+				s[index] = s[index] - 32;
+			}
 		}
-		i++;
-	}
-	return (0);
-}
-
-/**
- * cap_string - Met en majuscule la première lettre de chaque mot.
- * @str: La chaîne de caractères à modifier.
- *
- * Return: Un pointeur vers la chaîne modifiée.
- */
-char *cap_string(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		/* Si c'est le premier caractère et qu'il est minuscule */
-		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
-		{
-			str[i] = str[i] - 32;
-		}
-		/* Si le caractère précédent est un séparateur et l'actuel est minuscule */
-		else if (is_separator(str[i - 1]) && (str[i] >= 'a' && str[i] <= 'z'))
-		{
-			str[i] = str[i] - 32;
-		}
-		i++;
+		index++;
 	}
 
-	return (str);
+	return (s);
 }
