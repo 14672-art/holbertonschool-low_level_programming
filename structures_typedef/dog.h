c7a0db6a76247fbe1d0c2a12804f8bacf2108cab
maksoud <14672@holbertonstudents.com>
2026-07-09 10:38:36 +0200

    popy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A struct that stores basic information about a dog.
+ * @name: Pointer to a char array representing the dog's name.
+ * @age: Float representing the dog's age.
+ * @owner: Pointer to a char array representing the owner's name.
+ *
+ * Description: This structure groups the essential attributes of a dog,
+ * including its identity, age, and who it belongs to.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
