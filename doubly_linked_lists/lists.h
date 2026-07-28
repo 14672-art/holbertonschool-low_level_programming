702323bcd145f04ebcedaada7da713453eb3529f
Bilal <14672@holbertonstudents.com>
2026-07-28 14:14:30 +0200

    Fix lists.h prototypes

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -4,12 +4,13 @@
 #include <stddef.h>
 
 /**
- * struct dlistint_s - liste doublement chaînée
- * @n: entier
- * @prev: pointe vers le nœud précédent
- * @next: pointe vers le nœud suivant
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
  *
- * Description: structure de nœud pour liste doublement chaînée
+ * Description: doubly linked list node structure
+ * 
  */
 typedef struct dlistint_s
 {
@@ -20,6 +21,7 @@ typedef struct dlistint_s
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 #endif /* LISTS_H */
