ea855b52c0ae150eb6082a52b0cdc15362d199cf
Bilal <14672@holbertonstudents.com>
2026-07-28 11:57:30 +0200

    Add 1-dlistint_len.c

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - renvoie le nombre d'éléments dans une liste dlistint_t
+ * @h: pointeur vers la tête de la liste
+ *
+ * Return: le nombre de nœuds dans la liste
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
